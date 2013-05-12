b8d54c57508c169e38e6a7c8d73d7ecc2e9bbe16
sparks <sparks@doesnotexist.com>
2013-05-12 14:47:39 +0100

    Created DataGeneratorException

---
+++ DataGenerator/src/datagenerator/exceptions/DataGeneratorException.java
@@ -0,0 +1,25 @@
+package datagenerator.exceptions;
+
+public class DataGeneratorException extends Exception {
+	
+	/**
+	 * Eclipse Generated Serial Version UID
+	 */
+	private static final long serialVersionUID = -8094601251579976030L;
+
+	public DataGeneratorException() {
+		super();
+	}
+	
+	public DataGeneratorException(String msg) {
+		super(msg);
+	}
+	
+	public DataGeneratorException(Throwable throwable) {
+		super(throwable);
+	}
+	
+	public DataGeneratorException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+}
